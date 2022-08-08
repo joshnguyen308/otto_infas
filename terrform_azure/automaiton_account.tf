f18683c36c6637d2951ee8446d7eed1b6e02aa09
“Nhan <nhan.nguyen@ottoint.com>
2022-08-09 00:46:45 +0700

    update

--- terrform_azure/automaiton_account.tf
+++ terrform_azure/automaiton_account.tf
@@ -1,13 +1,13 @@
 
-resource "azurerm_resource_group" "AzureAutomationAccount" {
-  name     = "southeastasia-rg-fin-AzureAutomationAccount"
-  location = "Southeast Asia"
-}
+# resource "azurerm_resource_group" "AzureAutomationAccount" {
+#   name     = "southeastasia-rg-fin-AzureAutomationAccount"
+#   location = "Southeast Asia"
+# }
 
 resource "azurerm_automation_account" "AutomationTurnOnOffVMs" {
-  name                = "AutomationTurnOnOffVMs"
-  location            = azurerm_resource_group.AzureAutomationAccount.location
-  resource_group_name = azurerm_resource_group.AzureAutomationAccount.name
+  name                = var.automation_account_name
+  location            = var.automation_account_region
+  resource_group_name = var.automation_account_resourcesgroup
   sku_name            = "Basic"
   identity {
     type = "SystemAssigned"
@@ -20,8 +20,8 @@ resource "azurerm_automation_account" "AutomationTurnOnOffVMs" {
 
 resource "azurerm_automation_runbook" "VMsTurnOffScript" {
   name                    = "VMsTurnOffScript"
-  location                = azurerm_resource_group.AzureAutomationAccount.location
-  resource_group_name     = azurerm_resource_group.AzureAutomationAccount.name
+  location                = var.automation_account_region
+  resource_group_name     = var.automation_account_resourcesgroup
   automation_account_name = azurerm_automation_account.AutomationTurnOnOffVMs.name
   log_verbose             = "false"
   log_progress            = "false"
@@ -35,8 +35,8 @@ resource "azurerm_automation_runbook" "VMsTurnOffScript" {
 
 resource "azurerm_automation_runbook" "VMsTurnOnScript" {
   name                    = "VMsTurnOnScript"
-  location                = azurerm_resource_group.AzureAutomationAccount.location
-  resource_group_name     = azurerm_resource_group.AzureAutomationAccount.name
+  location                = var.automation_account_region
+  resource_group_name     = var.automation_account_resourcesgroup
   automation_account_name = azurerm_automation_account.AutomationTurnOnOffVMs.name
   log_verbose             = "false"
   log_progress            = "false"
@@ -51,7 +51,7 @@ resource "azurerm_automation_runbook" "VMsTurnOnScript" {
 
 resource "azurerm_automation_schedule" "Group2TurnOnTimeFrame" {
   name                    = "Group2TurnOnTimeFrame"
-  resource_group_name     = azurerm_resource_group.AzureAutomationAccount.name
+  resource_group_name     = var.automation_account_resourcesgroup
   automation_account_name = azurerm_automation_account.AutomationTurnOnOffVMs.name
   frequency               = "Week"
   interval                = 1
@@ -62,7 +62,7 @@ resource "azurerm_automation_schedule" "Group2TurnOnTimeFrame" {
 }
 
 resource "azurerm_automation_job_schedule" "LinkScheduleToRunbook1" {
-  resource_group_name     = azurerm_resource_group.AzureAutomationAccount.name
+  resource_group_name     = var.automation_account_resourcesgroup
   automation_account_name = azurerm_automation_account.AutomationTurnOnOffVMs.name
   schedule_name           = azurerm_automation_schedule.Group2TurnOnTimeFrame.name
   runbook_name            = azurerm_automation_runbook.VMsTurnOnScript.name
@@ -71,7 +71,7 @@ resource "azurerm_automation_job_schedule" "LinkScheduleToRunbook1" {
 
 resource "azurerm_automation_schedule" "Group2TurnOffTimeFrame" {
   name                    = "Group2TurnOffTimeFrame"
-  resource_group_name     = azurerm_resource_group.AzureAutomationAccount.name
+  resource_group_name     = var.automation_account_resourcesgroup
   automation_account_name = azurerm_automation_account.AutomationTurnOnOffVMs.name
   frequency               = "Week"
   interval                = 1
@@ -82,7 +82,7 @@ resource "azurerm_automation_schedule" "Group2TurnOffTimeFrame" {
 }
 
 resource "azurerm_automation_job_schedule" "LinkScheduleToRunbook2" {
-  resource_group_name     = azurerm_resource_group.AzureAutomationAccount.name
+  resource_group_name     = var.automation_account_resourcesgroup
   automation_account_name = azurerm_automation_account.AutomationTurnOnOffVMs.name
   schedule_name           = azurerm_automation_schedule.Group2TurnOffTimeFrame.name
   runbook_name            = azurerm_automation_runbook.VMsTurnOffScript.name
---
+++ terrform_azure/variables.tf
@@ -0,0 +1,12 @@
+variable "automation_account_name" {
+  default = "AutomationTurnOnOffVMs"
+}
+
+variable "automation_account_region" {
+  default = "Southeast Asia"
+}
+
+variable "automation_account_resourcesgroup" {
+  default = "southeastasia-rg-fin-dev-NAV-01"
+}
+
